f2e4b77907f646cac5ee597d972bf099c11aeb68
k08 <Fynn-ruebenach@t-online.de>
2019-11-07 20:58:27 +0100

    first release build
    don't expect too much

---
+++ ADBinstaller/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ADBinstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.btnDownload = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.linkTaC = new System.Windows.Forms.LinkLabel();
+            this.checkSystemWide = new MaterialSkin.Controls.MaterialCheckBox();
+            this.checkStartMenu = new MaterialSkin.Controls.MaterialCheckBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Roboto", 10F);
+            this.lblVersion.ForeColor = System.Drawing.Color.White;
+            this.lblVersion.Location = new System.Drawing.Point(12, 79);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(69, 18);
+            this.lblVersion.TabIndex = 0;
+            this.lblVersion.Text = "lblVersion";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.AutoSize = true;
+            this.btnDownload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDownload.Depth = 0;
+            this.btnDownload.Icon = null;
+            this.btnDownload.Location = new System.Drawing.Point(12, 286);
+            this.btnDownload.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Primary = true;
+            this.btnDownload.Size = new System.Drawing.Size(185, 36);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download and install";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.BtnDownload_Click);
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Roboto", 9F);
+            this.lblDesc.ForeColor = System.Drawing.Color.White;
+            this.lblDesc.Location = new System.Drawing.Point(12, 105);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(325, 75);
+            this.lblDesc.TabIndex = 2;
+            this.lblDesc.Text = "This tool is made to install the latest Android \r\nplatform-tools containing ADB a" +
+    "nd Fastboot.\r\nThe files are downloaded from the official Android Servers. \r\n\r\nBy" +
+    " downloading, you accept \r\n";
+            // 
+            // linkTaC
+            // 
+            this.linkTaC.AutoSize = true;
+            this.linkTaC.Font = new System.Drawing.Font("Roboto Medium", 9F);
+            this.linkTaC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.linkTaC.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkTaC.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.linkTaC.Location = new System.Drawing.Point(12, 187);
+            this.linkTaC.Name = "linkTaC";
+            this.linkTaC.Size = new System.Drawing.Size(174, 16);
+            this.linkTaC.TabIndex = 3;
+            this.linkTaC.TabStop = true;
+            this.linkTaC.Text = "Google\'s Terms and Conditions";
+            this.linkTaC.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkTaC_LinkClicked);
+            // 
+            // checkSystemWide
+            // 
+            this.checkSystemWide.AutoSize = true;
+            this.checkSystemWide.Depth = 0;
+            this.checkSystemWide.Font = new System.Drawing.Font("Roboto", 10F);
+            this.checkSystemWide.Location = new System.Drawing.Point(12, 212);
+            this.checkSystemWide.Margin = new System.Windows.Forms.Padding(0);
+            this.checkSystemWide.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.checkSystemWide.MouseState = MaterialSkin.MouseState.HOVER;
+            this.checkSystemWide.Name = "checkSystemWide";
+            this.checkSystemWide.Ripple = true;
+            this.checkSystemWide.Size = new System.Drawing.Size(149, 30);
+            this.checkSystemWide.TabIndex = 5;
+            this.checkSystemWide.Text = "Install system-wide";
+            this.checkSystemWide.UseVisualStyleBackColor = true;
+            // 
+            // checkStartMenu
+            // 
+            this.checkStartMenu.AutoSize = true;
+            this.checkStartMenu.Checked = true;
+            this.checkStartMenu.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkStartMenu.Depth = 0;
+            this.checkStartMenu.Font = new System.Drawing.Font("Roboto", 10F);
+            this.checkStartMenu.Location = new System.Drawing.Point(12, 242);
+            this.checkStartMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.checkStartMenu.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.checkStartMenu.MouseState = MaterialSkin.MouseState.HOVER;
+            this.checkStartMenu.Name = "checkStartMenu";
+            this.checkStartMenu.Ripple = true;
+            this.checkStartMenu.Size = new System.Drawing.Size(140, 30);
+            this.checkStartMenu.TabIndex = 6;
+            this.checkStartMenu.Text = "Add to start menu";
+            this.checkStartMenu.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Roboto", 9F);
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(203, 297);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(67, 15);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Status: Idle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(350, 337);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.checkStartMenu);
+            this.Controls.Add(this.checkSystemWide);
+            this.Controls.Add(this.linkTaC);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lblVersion);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 337);
+            this.MinimumSize = new System.Drawing.Size(350, 337);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADB Installer by @K08germany (TG)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.LinkLabel linkTaC;
+        private MaterialSkin.Controls.MaterialCheckBox checkSystemWide;
+        private MaterialSkin.Controls.MaterialCheckBox checkStartMenu;
+        private MaterialSkin.Controls.MaterialRaisedButton btnDownload;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ ADBinstaller/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using MaterialSkin;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+
+
+namespace ADBinstaller
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Init MaterialSkin
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Red500, Accent.Red200, TextShade.WHITE);
+
+        }
+
+        private void LinkTaC_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://developer.android.com/studio/terms");
+        }
+
+        private void BtnDownload_Click(object sender, EventArgs e)
+        {
+            var webCl = new WebClient();
+            var instDir = @"C:\Program Files (x86)\platform-tools";
+            var cDir = @"C:\Program Files (x86)";
+            var zipName = "platform-tools.zip";
+            lblStatus.Text = "Status: Downloading";
+
+            try
+            {
+                webCl.DownloadFile("https://dl.google.com/android/repository/platform-tools-latest-windows.zip", zipName);
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Error: " + System.Environment.NewLine + System.Environment.NewLine + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lblStatus.Text = "Status: Idle";
+
+            }
+            
+            if (System.IO.File.Exists(zipName))
+            {
+                lblStatus.Text = "Status: Extracting...";
+                System.IO.DirectoryInfo di = new DirectoryInfo(instDir);
+
+                if (File.Exists(instDir + @"\adb.exe"))
+                    {
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                    file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                    dir.Delete(true);
+                }}
+
+                ZipFile.ExtractToDirectory(zipName, cDir);
+            }
+
+            if (checkSystemWide.Checked = true)
+            {
+                lblStatus.Text = "Status: Adding EnvVars...";
+                const string name = "PATH";
+                string pathvar = System.Environment.GetEnvironmentVariable(name);
+                var value = pathvar + @";C:\Program Files (x86)\platform-tools";
+                var target = EnvironmentVariableTarget.Machine;
+                System.Environment.SetEnvironmentVariable(name, value, target);
+            }
+            if (checkStartMenu.Checked = true)
+            {
+                string path = instDir + @"\open.exe";
+                lblStatus.Text = "Status: Add to Start";
+                webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/open.exe", path);
+
+                string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+                string appStartMenuPath = commonStartMenuPath + @"\Programs\";
+                webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/ADB%20and%20Fastboot.lnk", appStartMenuPath + "ADB and Fastboot.lnk");
+
+
+            }
+        Finish:
+            MessageBox.Show("Done", "ADBInstaller", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var installed = @"C:\Program Files (x86)\platform-tools\adb.exe";
+            if (File.Exists(installed))
+            {
+                lblVersion.Text = "Installed: Yes (" + File.GetLastWriteTime(installed) + ")";
+            }
+            else
+            {
+                lblVersion.Text = "Installed: None";
+            }
+        }
+    }
+}
