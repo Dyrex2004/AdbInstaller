c2aa92d0e14823c1a1ef7262c8ec82bf5a787b97
K08official <Fynn-ruebenach@t-online.de>
2019-11-12 19:04:19 +0100

    removed unnecessary libs

--- ADBinstaller/Form1.Designer.cs
+++ ADBinstaller/Form1.Designer.cs
@@ -125,9 +125,9 @@
             this.label1.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(244, 26);
+            this.label1.Size = new System.Drawing.Size(125, 26);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Convenient ADB Installer";
+            this.label1.Text = "AdbInstaller";
             // 
             // label2
             // 
