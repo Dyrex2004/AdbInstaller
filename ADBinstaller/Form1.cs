41ee3b597f9cd73d8c9dbba657d10785a8a8243c
K08official <Fynn-ruebenach@t-online.de>
2019-11-12 18:55:57 +0100

    big update; added ifs and removed materialskin, allowing the program to only run with the exe and no additional files. Isn't that cool?

--- ADBinstaller/Form1.cs
+++ ADBinstaller/Form1.cs
@@ -10,94 +10,30 @@ using System.Windows.Forms;
 using System.Net;
 using System.IO;
 using System.IO.Compression;
-using MaterialSkin;
-using MaterialSkin.Animations;
-using MaterialSkin.Controls;
+// using MaterialSkin;
+// using MaterialSkin.Animations;
+// using MaterialSkin.Controls;
 
 
 namespace ADBinstaller
 {
-    public partial class Form1 : MaterialForm
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
 
             // Init MaterialSkin
-            var materialSkinManager = MaterialSkinManager.Instance;
-            materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
-            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Red500, Accent.Red200, TextShade.WHITE);
+            // var materialSkinManager = MaterialSkinManager.Instance;
+            // materialSkinManager.AddFormToManage(this);
+            // materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            // materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Red500, Accent.Red200, TextShade.WHITE);
 
         }
 
         private void LinkTaC_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://developer.android.com/studio/terms");
-        }
-
-        private void BtnDownload_Click(object sender, EventArgs e)
-        {
-            var webCl = new WebClient();
-            var instDir = @"C:\Program Files (x86)\platform-tools";
-            var cDir = @"C:\Program Files (x86)";
-            var zipName = "platform-tools.zip";
-            lblStatus.Text = "Status: Downloading";
-
-            try
-            {
-                webCl.DownloadFile("https://dl.google.com/android/repository/platform-tools-latest-windows.zip", zipName);
-            }
-            catch (WebException ex)
-            {
-                MessageBox.Show("Error: " + System.Environment.NewLine + System.Environment.NewLine + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                lblStatus.Text = "Status: Idle";
-
-            }
-            
-            if (System.IO.File.Exists(zipName))
-            {
-                lblStatus.Text = "Status: Extracting...";
-                System.IO.DirectoryInfo di = new DirectoryInfo(instDir);
-
-                if (File.Exists(instDir + @"\adb.exe"))
-                    {
-                    foreach (FileInfo file in di.GetFiles())
-                    {
-                    file.Delete();
-                    }
-                    foreach (DirectoryInfo dir in di.GetDirectories())
-                    {
-                    dir.Delete(true);
-                }}
-
-                ZipFile.ExtractToDirectory(zipName, cDir);
-            }
-
-            if (checkSystemWide.Checked = true)
-            {
-                lblStatus.Text = "Status: Adding EnvVars...";
-                const string name = "PATH";
-                string pathvar = System.Environment.GetEnvironmentVariable(name);
-                var value = pathvar + @";C:\Program Files (x86)\platform-tools";
-                var target = EnvironmentVariableTarget.Machine;
-                System.Environment.SetEnvironmentVariable(name, value, target);
-            }
-            if (checkStartMenu.Checked = true)
-            {
-                string path = instDir + @"\open.exe";
-                lblStatus.Text = "Status: Add to Start";
-                webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/open.exe", path);
-
-                string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
-                string appStartMenuPath = commonStartMenuPath + @"\Programs\";
-                webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/ADB%20and%20Fastboot.lnk", appStartMenuPath + "ADB and Fastboot.lnk");
-
-
-            }
-        Finish:
-            MessageBox.Show("Done, it's recommended to restart your PC now. Problems? Message me on TG, @K08germany.", "ADBInstaller", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -111,6 +47,95 @@ namespace ADBinstaller
             {
                 lblVersion.Text = "Installed: None";
             }
+        }
+
+        private void BtnDownload_Click_1(object sender, EventArgs e)
+        {
+            {
+                var webCl = new WebClient();
+                var instDir = @"C:\Program Files (x86)\platform-tools";
+                var cDir = @"C:\Program Files (x86)";
+                var zipName = "platform-tools.zip";
+                lblStatus.Text = "Status: Downloading";
+
+                try
+                {
+                    webCl.DownloadFile("https://dl.google.com/android/repository/platform-tools-latest-windows.zip", zipName);
+                }
+                catch (WebException ex)
+                {
+                    MessageBox.Show("Error: " + System.Environment.NewLine + System.Environment.NewLine + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    lblStatus.Text = "Status: Idle";
+                    goto Finish_error;
+                }
+
+                if (System.IO.File.Exists(zipName))
+                {
+                    lblStatus.Text = "Status: Extracting...";
+                    System.IO.DirectoryInfo di = new DirectoryInfo(instDir);
+
+                    if (File.Exists(instDir + @"\adb.exe"))
+                    {
+                        foreach (FileInfo file in di.GetFiles())
+                        {
+                            file.Delete();
+                        }
+                        foreach (DirectoryInfo dir in di.GetDirectories())
+                        {
+                            dir.Delete(true);
+                        }
+                    }
+
+                    ZipFile.ExtractToDirectory(zipName, cDir);
+                }
+
+                if (System.Environment.GetEnvironmentVariable("path").Contains(instDir))
+                {
+                }
+                else if (checkSystemWide.Checked == true)
+                {
+                    lblStatus.Text = "Status: Adding EnvVars...";
+                    const string name = "PATH";
+                    string pathvar = System.Environment.GetEnvironmentVariable(name);
+                    var value = pathvar + @";C:\Program Files (x86)\platform-tools";
+                    var target = EnvironmentVariableTarget.Machine;
+                    System.Environment.SetEnvironmentVariable(name, value, target);
+                }
+
+                if (checkStartMenu.Checked == true)
+                {
+                    string path = instDir + @"\open.exe";
+                    lblStatus.Text = "Status: Add to Start";
+                    webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/open.exe", path);
+
+                    string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+                    string appStartMenuPath = commonStartMenuPath + @"\Programs\";
+                    webCl.DownloadFile("https://github.com/K08official/AdbInstaller/raw/master/Files/ADB%20and%20Fastboot.lnk", appStartMenuPath + "ADB and Fastboot.lnk");
+
+
+                }
+            Finish:
+                MessageBox.Show("Done, it's recommended to restart your PC now. Problems? Message me on TG, @K08germany.", "ADBInstaller", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                goto Finish_Done;
+            Finish_error:
+                MessageBox.Show("Installation failed!", "ADBInstaller", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+        Finish_Done:;
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (System.Environment.GetEnvironmentVariable("path").Contains(@"C:\Program Files (x86)\platform-tools"))
+            {
+                MessageBox.Show("contains" + @"C:\Program Files (x86)\platform-tools");
+            }
+            else { MessageBox.Show("gay"); }
         }
     }
 }
